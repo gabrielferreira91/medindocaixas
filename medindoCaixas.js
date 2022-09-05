45c6bb45882e0c061972383db6f55f82dd2dfa04
gabrielferreira91 <66332615+gabrielferreira91@users.noreply.github.com>
2022-09-04 21:31:48 -0300

    Add valor a caixa

--- medindoCaixas.js
+++ medindoCaixas.js
@@ -1,139 +1,220 @@
-let inputLengthBox = document.getElementById('lengthBox');
-let inputWidthBox = document.getElementById('widthBox');
-let inputHeightBox = document.getElementById('heightBox');
+const transpasseNormal = 10
+const transpasse30 = 60
+const abaEnvoltorio = 80
+const abaMaleta = 70
+const abaEnvoltorio01Tampa = 60
+const somaAltura = 5
 
+let inputLengthBox = document.getElementById('lengthBox')
+let inputWidthBox = document.getElementById('widthBox')
+let inputHeightBox = document.getElementById('heightBox')
 let totalLength = document.getElementById('totalLength')
 let totalWidth = document.getElementById('totalWidth')
 
+let inputGramatura = document.getElementById('gramatura')
+let inputValorKg = document.getElementById('valorKg')
+let totalValue = document.getElementById('resultValue')
+let totalRs = document.getElementById('resultR')
+
 let tipoDeCaixa = document.getElementById('tiposDeCaixa')
 
-const transpasseNormal = 10;
-const transpasse30 = 60;
-const abaEnvoltorio = 80;
-const abaMaleta = 70;
-const abaEnvoltorio01Tampa = 60;
-const somaAltura = 5;
+let buttonValue = document.getElementById('calcValor')
 
 let resultBox = []
+let isInvalid = false
+let isInvalidValue = false
 
-let isInvalid = false;
+function typeOfBox(boxes) {
+  let lengthBox = parseInt(inputLengthBox.value)
+  let widthBox = parseInt(inputWidthBox.value)
+  let heightBox = parseInt(inputHeightBox.value)
 
-function typeOfBox(boxes){
-    
-    let lengthBox = parseInt(inputLengthBox.value);
-    let widthBox = parseInt(inputWidthBox.value);
-    let heightBox = parseInt(inputHeightBox.value);
+  if (
+    isInvalidFunction(lengthBox) ||
+    isInvalidFunction(widthBox) ||
+    isInvalidFunction(heightBox)
+  ) {
+    isInvalid = true
+    addClass()
+  } else {
+    isInvalid = false
+    removeClass()
 
-    if (lengthBox <= 0 || widthBox <= 0 || heightBox <= 0 || isNaN(lengthBox) || isNaN(widthBox) || isNaN(heightBox)){
-        isInvalid = true;
-    }   else {
-        
+    switch (boxes) {
+      case 'maleta':
+        calcMaleta(lengthBox, widthBox, heightBox)
+        break
 
-        isInvalid = false;
+      case 'maletaTTotal':
+        calcMaletaTTotal(lengthBox, widthBox, heightBox)
+        break
 
-        switch(boxes){
-            case 'maleta':
-                calcMaleta(lengthBox, widthBox, heightBox);
-                console.log('maleta');
-            break;
-                
-            case 'maletaTTotal':
-                calcMaletaTTotal(lengthBox, widthBox, heightBox);
-                console.log('maletaTTOTAL');
-            break;
+      case 'maletaT030':
+        calcMaletaT30(lengthBox, widthBox, heightBox)
+        break
 
-            case 'maletaT030':
-                calcMaletaT30(lengthBox, widthBox, heightBox);
-                console.log('maletaT030');
-            break;
+      case 'env1tampa':
+        calcEnv01Tampa(lengthBox, widthBox, heightBox)
+        break
 
-            case 'env1tampa':
-                calcEnv01Tampa(lengthBox, widthBox, heightBox);
-                console.log('ENV 01 TAMPA');
-            break;
+      case 'envNormal':
+        calcEnvNormal(lengthBox, widthBox, heightBox)
+        break
 
-            case 'envNormal':
-                calcEnvNormal(lengthBox, widthBox, heightBox);
-                console.log('Env NORMAL');
-            break;
-
-            case 'envT030':
-                calcEnvTransp30(lengthBox, widthBox, heightBox);
-                console.log('ENV T 030');
-            break;
-                
-        }
+      case 'envT030':
+        calcEnvTransp30(lengthBox, widthBox, heightBox)
+        break
     }
+  }
 }
 //Calculando medidas de caixas
 
-function calcMaleta(length,width,height) {
-    const riscador = (width+transpasseNormal)/2 + (height+somaAltura) + (width+transpasseNormal)/2;
-    const impressora = length + width + length + width + abaMaleta;
+function calcMaleta(length, width, height) {
+  const riscador =
+    (width + transpasseNormal) / 2 +
+    (height + somaAltura) +
+    (width + transpasseNormal) / 2
+  const impressora = length + width + length + width + abaMaleta
 
-    resultBox = [riscador , impressora]
-    return resultBox
+  resultBox = [riscador, impressora]
+  return resultBox
 }
 
-function calcMaletaTTotal(length, width, height){
-    const riscador = width + (height+somaAltura) + width
-    const impressora = length + width + length + width + abaMaleta;
-    
-    resultBox = [riscador , impressora]
-    return resultBox
+function calcMaletaTTotal(length, width, height) {
+  const riscador = width + (height + somaAltura) + width
+  const impressora = length + width + length + width + abaMaleta
+
+  resultBox = [riscador, impressora]
+  return resultBox
 }
 
-function calcMaletaT30(length,width,height) {
-    const riscador = (width + transpasse30)/2 + (height+somaAltura) + (width + transpasse30)/2;
-    const impressora = length + width + length + width + abaMaleta;
+function calcMaletaT30(length, width, height) {
+  const riscador =
+    (width + transpasse30) / 2 +
+    (height + somaAltura) +
+    (width + transpasse30) / 2
+  const impressora = length + width + length + width + abaMaleta
 
-    resultBox = [riscador , impressora]
-    return resultBox
+  resultBox = [riscador, impressora]
+  return resultBox
 }
 
 function calcEnvNormal(length, width, height) {
-    const riscador = (width+transpasseNormal)/2 + (height+somaAltura) + (width+somaAltura) + (height+somaAltura) + (width+transpasseNormal)/2;
-    const impressora = abaEnvoltorio + height + length + height + abaEnvoltorio;
+  const riscador =
+    (width + transpasseNormal) / 2 +
+    (height + somaAltura) +
+    (width + somaAltura) +
+    (height + somaAltura) +
+    (width + transpasseNormal) / 2
+  const impressora = abaEnvoltorio + height + length + height + abaEnvoltorio
 
-    resultBox = [riscador , impressora]
-    return resultBox
+  resultBox = [riscador, impressora]
+  return resultBox
 }
 
 function calcEnv01Tampa(length, width, height) {
-    const riscador = (width+somaAltura) + (height+somaAltura) + (width+somaAltura) + (height+somaAltura) + abaEnvoltorio01Tampa;
-    const impressora = abaEnvoltorio + height + length + height + abaEnvoltorio;
+  const riscador =
+    width +
+    somaAltura +
+    (height + somaAltura) +
+    (width + somaAltura) +
+    (height + somaAltura) +
+    abaEnvoltorio01Tampa
+  const impressora = abaEnvoltorio + height + length + height + abaEnvoltorio
 
-    resultBox = [riscador , impressora]
-    return resultBox
+  resultBox = [riscador, impressora]
+  return resultBox
 }
 
 function calcEnvTransp30(length, width, height) {
-    const riscador =  (width+transpasse30)/2 + (height+somaAltura) + (width+somaAltura) + (height+somaAltura) + (width+transpasse30)/2;
-    const impressora = abaEnvoltorio + height + length + height + abaEnvoltorio;
+  const riscador =
+    (width + transpasse30) / 2 +
+    (height + somaAltura) +
+    (width + somaAltura) +
+    (height + somaAltura) +
+    (width + transpasse30) / 2
+  const impressora = abaEnvoltorio + height + length + height + abaEnvoltorio
 
-    resultBox = [riscador , impressora]
-    return resultBox
+  resultBox = [riscador, impressora]
+  return resultBox
 }
 
+
+function isInvalidFunction(valor) {
+  if (valor <= 0 || isNaN(valor)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function finalValue(comp, larg, gramatura, valor) {
+  let area = (comp * larg) / 1000
+  let result = (gramatura * area * valor) / 1000
+  
+  return result
+}
+
+function gramatura(gram) {
+  gram > 1 ? (gram = gram / 1000) : gram
+  return gram
+}
+
+function setTimes(resultLength, resultWidth) {
+  totalLength.textContent = resultLength
+  totalWidth.textContent = resultWidth
+}
+
+function setValue(resultValue) {
+  totalValue.textContent = resultValue.toFixed(2)
+}
+
+function msgError() {
+  totalLength.textContent = 'VALOR '
+  totalWidth.textContent = 'INVÁLIDO'
+}
+
+function msgErrorValue() {
+  totalRs.textContent = ''
+  totalValue.textContent = 'VALOR INVÁLIDO'
+}
+
+function removeClass() {
+  buttonValue.disabled = false
+  buttonValue.classList.remove('disabled')
+}
+
+function addClass() {
+  buttonValue.disabled = true
+  buttonValue.classList.add('disabled')
+}
 
 // FUNCTION OF BUTTON
 
-function getMeasure(){
-    tipoDeCaixa = document.getElementById('tiposDeCaixa').value
-    
-    typeOfBox(tipoDeCaixa);
-    isInvalid ? msgError() : setTimes(resultBox[0], resultBox[1]);
-    
+function getMeasure() {
+  tipoDeCaixa = document.getElementById('tiposDeCaixa').value
+
+  typeOfBox(tipoDeCaixa)
+  isInvalid ? msgError() : setTimes(resultBox[0], resultBox[1])
 }
 
-function setTimes(resultLength, resultWidth){
-    totalLength.textContent = resultLength 
-    totalWidth.textContent =  resultWidth
+function getValues() {
+  getMeasure()
+  let gramaturaCaixa = gramatura(parseFloat(inputGramatura.value))
+  let valorKg = parseFloat(inputValorKg.value)
 
-}
+  const valorFinal = finalValue(
+    resultBox[0],
+    resultBox[1],
+    gramaturaCaixa,
+    valorKg
+  )
 
+  if (isInvalidFunction(gramaturaCaixa) || isInvalidFunction(valorKg)) {
+    isInvalidValue = true
+  } else {
+    isInvalidValue = false
+  }
 
-function msgError(){
-    totalLength.textContent = 'VALOR ';
-    totalWidth.textContent = 'INVÁLIDO';
+  isInvalidValue ? msgErrorValue() : setValue(valorFinal)
 }
